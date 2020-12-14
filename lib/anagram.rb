045745f419f11d09fcf80e8c0202c54cc04372a0
Allie <alliecaton3@gmail.com>
2020-12-14 14:32:56 -0500

    commit tree fcb59fe88cfb0a43bfd51552bb19458b3add2baf

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end 
+
+    def match(words_array)
+        words_array.collect do | word |
+            if word.split("").sort == @word.split("").sort
+                word
+            end
+        end.flatten.compact 
+    end 
+
+end 
